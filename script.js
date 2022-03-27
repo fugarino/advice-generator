06379799435c6f2f24954b1905f12d4c8494f95d
Christian Fugarino <christian.fugarino.123@gmail.com>
2022-03-27 11:19:32 -0400

    finish files

---
+++ script.js
@@ -0,0 +1,22 @@
+// Selectors
+const adviceNumberElement = document.querySelector(".advice-number");
+const adviceTextElement = document.querySelector(".advice-text");
+const btn = document.querySelector("button");
+
+// Functions
+const handleClick = () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => handleData(data));
+};
+
+const handleData = (data) => {
+  const adviceNumber = data.slip.id;
+  const adviceText = data.slip.advice;
+  // Displaying Content
+  adviceNumberElement.textContent = `ADVICE #${adviceNumber}`;
+  adviceTextElement.textContent = `"${adviceText}"`;
+};
+
+// EventListeners
+btn.addEventListener("click", handleClick);
